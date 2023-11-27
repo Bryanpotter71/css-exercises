be68c12aa00647db7a99d063c416b4c354e700f1
bryanpotter71 <bryanromeo@gmail.com>
2023-11-26 19:06:05 -0600

    Enhance functionality with DOMContentLoaded and fix button event handlers

--- animation/02-pop-up/script.js
+++ animation/02-pop-up/script.js
@@ -1,12 +1,14 @@
-const openButton = document.getElementById('trigger-modal');
-const closeButton = document.getElementById('close-modal');
+document.addEventListener('DOMContentLoaded', (event) => {
+  const openButton = document.getElementById('trigger-modal');
+  const closeButton = document.getElementById('close-modal');
+  
+  function toggleModal() {
+    const modal = document.querySelector('.popup-modal');
+    const backdrop = document.querySelector('.backdrop');
+    modal.classList.toggle('show');
+    backdrop.classList.toggle('show');
+  }
 
-function toggleModal() {
-  const modal = document.querySelector('.popup-modal');
-  const backdrop = document.querySelector('.backdrop');
-  modal.classList.toggle('show');
-  backdrop.classList.toggle('show');
-}
-
-openButton.addEventListener('click', toggleModal);
-closeButton.addEventListener('click', toggleModal);
+  openButton.addEventListener('click', toggleModal);
+  closeButton.addEventListener('click', toggleModal);
+});

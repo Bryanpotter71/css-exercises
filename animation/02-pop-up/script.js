0ea551efef90da7eb6d5504e637b7f94eb0a056b
bryanpotter71 <bryanromeo@gmail.com>
2023-11-26 18:29:24 -0600

    Implement popup modal with open/close animations and styling

--- animation/02-pop-up/script.js
+++ animation/02-pop-up/script.js
@@ -2,9 +2,9 @@ const openButton = document.getElementById('trigger-modal');
 const closeButton = document.getElementById('close-modal');
 
 function toggleModal() {
-  const modalDiv = document.querySelector('.popup-modal');
+  const modal = document.querySelector('.popup-modal');
   const backdrop = document.querySelector('.backdrop');
-  modalDiv.classList.toggle('show');
+  modal.classList.toggle('show');
   backdrop.classList.toggle('show');
 }
 
